e8cdd91b731552ff8a798d710cfc6e10fdf92bb2
MiguelGuevara1071 <miguelguevara1071@gmail.com>
2023-12-16 17:15:42 -0500

    Subida de codigo

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 // Confugiracion de axios y ruta de conexion al backend, mediante uso de credenciales de acceso
+// const URL = "http://localhost:4000/api" // Url Desarrollo
+const URL = "https://unikasas-backend-prueba-dev-qckm.3.us-1.fl0.io/api"
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: URL,
     withCredentials: true
 })
 
