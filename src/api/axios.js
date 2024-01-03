ed5403148f8a4b415faff8b7b66719c3722b5762
MiguelGuevara1071 <miguelguevara1071@gmail.com>
2024-01-03 12:03:43 -0500

    Ajuste rutas url

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
  * Confugiracion de axios y ruta de conexion al backend, mediante uso de credenciales de acceso,
  */
 const instance = axios.create({
-    baseURL: 'https://bakend-unikasas-2.onrender.com/api',
+    baseURL: 'http://localhost:4000/api',
     withCredentials: true
 })
 
