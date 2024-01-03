afd82463d55621d7d6579393e82930cdfe800b66
MiguelGuevara1071 <miguelguevara1071@gmail.com>
2024-01-02 19:22:56 -0500

    Modificacion url backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
  * Confugiracion de axios y ruta de conexion al backend, mediante uso de credenciales de acceso,
  */
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://bakend-unikasas-2.onrender.com/api',
     withCredentials: true
 })
 
